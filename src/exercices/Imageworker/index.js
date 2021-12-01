98d67842ab76d849412eae65f903265c5b83e780
lmalucell001 <laurent.malucelli@mmibordeaux.com>
2021-12-01 03:19:04 +0100

    style dynamique

--- src/exercices/Imageworker/index.js
+++ src/exercices/Imageworker/index.js
@@ -5,6 +5,8 @@ import nalfgas from './mousqueton.jpg';
 
 const Grille= () => {
 
+  
+
   return (
 <div className={styles.container}>
     <div className={styles.subContainer}>
@@ -16,7 +18,7 @@ const Grille= () => {
         <img src={nalfgas} className={styles.image} height="200px" width="200px" alt="nalfgas"/>
         </div>
     </div>
-    <div className={styles.subContainer}>
+    <div className={styles.Container2}>
         <div className={styles.frame1}>
         <img src={nalfgas} className={styles.image} height="200px" width="200px" alt="nalfgas"/>
         </div>
